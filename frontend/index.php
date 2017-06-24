c589457d7b317df39ed620a16f2357b500ebc32d
Juan Adasme <ju.adasme@alumnos.duoc.cl>
2017-06-24 01:10:10 -0300

    Add architecture.

---
+++ frontend/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Consultorio Municipal de Tetengo</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <script src="js/jquery-3.2.1.min.js" ></script>
+        <script src="js/jquery.Rut.js"></script>
+        
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
